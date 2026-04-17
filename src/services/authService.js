7c68d3b40af4edd5068b88067c416fc94abe6b39
Mohammed Eid <meid5170@gmail.com>
2026-04-17 22:52:04 +0200

    make auth fake بالكامل

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,5 +1,46 @@
-import api from "./api"
+// import api from "./api"
 
-export const login = (data) => api.post("/auth/login", data)
+// export const login = (data) => api.post("/auth/login", data)
 
-export const register = (data) => api.post("/auth/register", data)
+// export const register = (data) => api.post("/auth/register", data)
+
+
+export const login = async (data) => {
+  console.log("FAKE LOGIN:", data)
+
+  const fakeUser = {
+    name: "Test User",
+    email: data.email,
+  }
+
+  localStorage.setItem("token", "fake-token")
+  localStorage.setItem("user", JSON.stringify(fakeUser))
+
+  return {
+    data: {
+      success: true,
+      user: fakeUser,
+      token: "fake-token",
+    },
+  }
+}
+
+export const register = async (data) => {
+  console.log("FAKE REGISTER:", data)
+
+  const fakeUser = {
+    name: data.name,
+    email: data.email,
+  }
+
+  localStorage.setItem("token", "fake-token")
+  localStorage.setItem("user", JSON.stringify(fakeUser))
+
+  return {
+    data: {
+      success: true,
+      user: fakeUser,
+      token: "fake-token",
+    },
+  }
+}
